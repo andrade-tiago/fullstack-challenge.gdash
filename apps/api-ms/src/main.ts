d433b3eb6f6486cfca01d4512fbbd3a7ac1deffe
Tiago Andrade <tiagoandradedejoao@gmail.com>
2025-12-06 16:15:57 -0300

    chore(api-ms): enable CORS

--- apps/api-ms/src/main.ts
+++ apps/api-ms/src/main.ts
@@ -1,11 +1,13 @@
-import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
+import { NestFactory } from "@nestjs/core"
+import { AppModule } from "./app.module"
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule)
 
-  app.setGlobalPrefix('api')
+  app.setGlobalPrefix("api")
 
-  await app.listen(process.env.PORT ?? 3000);
+  app.enableCors()
+
+  await app.listen(process.env.PORT ?? 3000)
 }
 bootstrap();
